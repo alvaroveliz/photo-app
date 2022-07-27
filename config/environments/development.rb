8f0ae77ac5f9529d0e0a32fd838af03b6ffa36bc
Álvaro Véliz <alvaro.veliz@checkr.com>
2022-07-27 14:07:14 -0400

    fixing configuration for prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'alvaroveliz-photo-app.herokuapp.com', :protocol => 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "https://alvaroveliz-photo-app.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
