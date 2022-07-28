6e6c74f47623b2fe8794725611c2ca081214009f
Álvaro Véliz <alvaro.veliz@checkr.com>
2022-07-28 17:40:28 -0400

    fixing carrier wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,6 @@ if Rails.env.production?
       :aws_access_key_id      => Rails.application.credentials.dig(:aws, :access_key_id), 
       :aws_secret_access_key  => Rails.application.credentials.dig(:aws, :secret_access_key),
       :region                 => 'eu-east-1',
-      :host                   => 'alvaroveliz-photo-app.herokuapp.com',
-      :endpoint               => 'https://alvaroveliz-photo-app.herokuapp.com/'
     }
     config.fog_directory  = 'alvaroveliz-photoapp'
     config.fog_public     = true
